30a31d3aa0dd2755f8a15bfd2c2e5642d4fc3e23
Jorge Andres Echavarria Pardo <jorgean0827@gmail.com>
2025-08-11 20:06:04 -0500

    Agregar servicios y endpoints para consumir api

--- src/main/java/com/devsenior/tienda_mongodb/controller/ProductoController.java
+++ src/main/java/com/devsenior/tienda_mongodb/controller/ProductoController.java
@@ -11,6 +11,10 @@ import com.devsenior.tienda_mongodb.service.ProductoService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 
 @RestController
@@ -28,7 +32,26 @@ public class ProductoController {
 
     @PostMapping
     public Producto create(@RequestBody Producto producto) {
-        
         return productoService.guardar(producto);
     }
+
+    @GetMapping("/{id}")
+    public Producto getById(@PathVariable String id) {
+        return productoService.findById(id)
+                .orElseThrow(() -> new RuntimeException("Producto no encontrado con ID: " + id));
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable String id) {
+        productoService.delete(id);
+    }
+    
+    @PutMapping("/{id}")
+    public Producto updateById(@PathVariable String id, @RequestBody Producto productoActualizado) {
+        return productoService.update(id, productoActualizado);
+    }
+    
+    
+
+    
 }
--- src/main/java/com/devsenior/tienda_mongodb/service/ProductoService.java
+++ src/main/java/com/devsenior/tienda_mongodb/service/ProductoService.java
@@ -1,6 +1,7 @@
 package com.devsenior.tienda_mongodb.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,4 +22,30 @@ public class ProductoService {
     public Producto guardar(Producto producto){
         return productoRepository.save(producto);
     }
+
+    public Optional<Producto> findById(String id){
+        return productoRepository.findById(id);
+    }
+
+    public void delete(String id){
+        productoRepository.deleteById(id);
+    }
+
+    public Producto update(String id, Producto productoActualizado){
+        Optional<Producto> productoExistente = productoRepository.findById(id);
+        
+        if (productoExistente.isPresent()) {
+            Producto producto = productoExistente.get();
+            producto.setNombre(productoActualizado.getNombre());
+            producto.setPrecio(productoActualizado.getPrecio());
+            producto.setDescripcion(productoActualizado.getDescripcion());
+            producto.setStock(productoActualizado.getStock());
+            
+            return productoRepository.save(producto);
+        } else {
+            throw new RuntimeException("Producto no encontrado con id: " + id);
+        }
+    }
+
+    
 }
