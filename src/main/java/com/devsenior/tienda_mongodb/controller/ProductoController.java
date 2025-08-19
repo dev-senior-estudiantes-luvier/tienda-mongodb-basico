254fa158786fead35762f8593d77ddf431a2bf40
SnexDer <luvierorjuela@gmail.com>
2025-08-19 18:29:45 -0500

    modificacion de la gestion de errores en el controlador de productos

--- src/main/java/com/devsenior/tienda_mongodb/controller/ProductoController.java
+++ src/main/java/com/devsenior/tienda_mongodb/controller/ProductoController.java
@@ -3,32 +3,63 @@ package com.devsenior.tienda_mongodb.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsenior.tienda_mongodb.model.Producto;
 import com.devsenior.tienda_mongodb.service.ProductoService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
 
 @RestController
 @RequestMapping("/api/productos")
 public class ProductoController {
-
+    
     @Autowired
-    private ProductoService productoService;
-
+    private ProductoService service;
 
     @GetMapping
     public List<Producto> getAll() {
-        return productoService.listarTodos();
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Producto getById(@PathVariable String id) {
+        return service.findById(id).orElse(null);
     }
 
     @PostMapping
-    public Producto create(@RequestBody Producto producto) {
+    public Producto create(@RequestBody Producto product) {
+        return service.save(product);
+    }
+
+    @PutMapping("/{id}")
+    public Producto update(@PathVariable String id, @RequestBody Producto product) {
+        product.setId(id);
         
-        return productoService.guardar(producto);
+        Producto original = service.findById(id).orElse(null);
+        if (original != null) {
+            product.setFechaCreacion(original.getFechaCreacion());
+        }
+        return service.save(product);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable String id) {
+        service.delete(id);
+    }
+
+    @GetMapping("/falla/runtimeException")
+    public void arrojarErrorRuntimeException() {
+        throw new RuntimeException("Exception en en tiempo de ejecucion");
+    }
+
+    @GetMapping("/falla/exception")
+    public void arrojarErrorException() throws Exception {
+        throw new Exception();
     }
 }
